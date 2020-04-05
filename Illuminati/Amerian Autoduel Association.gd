a6460419f89d3a77ad0cdc8f7d5ca784fc4f6ad6
S.L <43483411+IamAono@users.noreply.github.com>
2020-04-05 10:34:16 -0700

    made script for American Autoduel Association

---
+++ Illuminati/Amerian Autoduel Association.gd
@@ -0,0 +1,22 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var power = 1;
+var resistance = 5;
+var income = 1;
+var up;
+var right = "out";
+var down;
+var left = "in";
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
