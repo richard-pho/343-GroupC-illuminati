9175bfe90f43ae079381fbd2367f46d7d6905ff1
richard-pho <55855050+richard-pho@users.noreply.github.com>
2020-04-18 23:41:48 -0700

    login button kind of works

--- Illuminati/HomeScreen.gd
+++ Illuminati/HomeScreen.gd
@@ -4,12 +4,15 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
+onready var username : LineEdit = $Menu/LoginInfo/VBoxContainer/LineEdit
+onready var notification : Label = $Menu/notification/Label
+onready var Lobby : Label = $Menu/Lobby/Popup/Label
+var usernamearray = [] 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in $Menu/Center/Options.get_children():
 		i.connect("pressed",self,"buttonpressed",[i.scene])
+	
 		
 func buttonpressed(scene):
 	get_tree().change_scene(scene)
@@ -18,3 +21,12 @@ func buttonpressed(scene):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Login_pressed():
+	notification.text = ""
+	if (usernamearray.has(username.text)):
+		notification.text = "username already in lobby"
+	else:
+		usernamearray.append(username.text)
+	
