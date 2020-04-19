3718587b0f957c38399159c2034b252533cb0929
Albert Truong <albertoe4788@gmail.com>
2020-04-19 15:31:28 -0700

    Login Lobby

--- Illuminati/HomeScreen.gd
+++ Illuminati/HomeScreen.gd
@@ -7,7 +7,10 @@ extends Control
 onready var username : LineEdit = $Menu/LoginInfo/VBoxContainer/LineEdit
 onready var notification : Label = $Menu/notification/Label
 onready var Lobby : Label = $Menu/Lobby/Popup/Label
+onready var LobbyList : ItemList = $Menu/LoginInfo/Login/LobbyList
 var usernamearray = [] 
+
+const MAX_LOBBY = 8
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in $Menu/Center/Options.get_children():
@@ -27,6 +30,9 @@ func _on_Login_pressed():
 	notification.text = ""
 	if (usernamearray.has(username.text)):
 		notification.text = "username already in lobby"
+	elif LobbyList.get_item_count() ==8:
+		notification.text = "Max Lobby Size Reached"
 	else:
 		usernamearray.append(username.text)
+		LobbyList.add_item(username.text)
 	
