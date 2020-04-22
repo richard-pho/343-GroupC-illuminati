befb017b7b2cd798a506362cca3634eb55f7e0cb
Albert Truong <albertoe4788@gmail.com>
2020-04-22 15:51:44 -0700

    globals

--- Illuminati/globals.gd
+++ Illuminati/globals.gd
@@ -1,15 +1,17 @@
-extends RichTextLabel
+extends Node
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+var array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var value = get_node("/root/globals")
-	print(value.array)
+	array.append(1)
+	# Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
