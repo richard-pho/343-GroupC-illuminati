a47a70b14d365df661e7d833b19bca80af39412a
S.L <43483411+IamAono@users.noreply.github.com>
2020-04-26 14:26:57 -0700

    fixed icard0

---
+++ Illuminati/Game/icard0.gd
@@ -0,0 +1,21 @@
+extends TextureRect
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var players = global.players # gets the array of players from global
+	var player0 = players[0] # gets the first player
+	player0.IlluminatiCard = players[0].IlluminatiCard.getname()
+	var path = "res://Card Assets Folder/" + player0.IlluminatiCard + ".JPG" # path of their Illuminati Card
+	var image = load(path) 
+	set_texture(image) # displays the Illuminati Card
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
