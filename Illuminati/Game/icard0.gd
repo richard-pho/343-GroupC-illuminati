1843f728053ae185ae4aae041d3c26c07d77d2f8
S.L <43483411+IamAono@users.noreply.github.com>
2020-04-26 14:27:59 -0700

    _

--- Illuminati/Game/icard0.gd
+++ Illuminati/Game/icard0.gd
@@ -8,6 +8,7 @@ extends TextureRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var global = get_node("/root/globals")
 	var players = global.players # gets the array of players from global
 	var player0 = players[0] # gets the first player
 	player0.IlluminatiCard = players[0].IlluminatiCard.getname()
