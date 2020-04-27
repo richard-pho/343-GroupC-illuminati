b9f0ba0fc2b4577ce6927b05b06fbd87e2ec6e05
richard-pho <55855050+richard-pho@users.noreply.github.com>
2020-04-27 15:22:36 -0700

    fixed object to string error

--- Illuminati/Game/Player_0.gd
+++ Illuminati/Game/Player_0.gd
@@ -15,16 +15,14 @@ func _ready():
 	# should work but illuminati cards are stored as strings
 	print(testarray)
 	player0 = testarray[0]
-	print(player0.getIlluminati())
-	#player0.getIlluminati().updateMoney(player0.getIlluminati().getincome())
-	#player0.IlluminatiCard.updateMoney(player0.IlluminatiCard.getincome())
+	player0.getIlluminati().updateMoney(player0.getIlluminati().getincome())
 	
 	# This will iterate through group cards and add income
-	#if (testarray[0].groupCards.size() > 0):
-		#for i in testarray[0].groupCards:
-			#i.money += i.income
+	if (testarray[0].groupCards.size() > 0):
+		for i in testarray[0].groupCards:
+			i.money += i.income
 			
-	#print(player0.IlluminatiCard.money)
+	print(player0.IlluminatiCard.money)
 
 
 func _on_endturn_pressed():
--- Illuminati/Game/icard0.gd
+++ Illuminati/Game/icard0.gd
@@ -10,9 +10,7 @@ extends TextureRect
 func _ready():
 	var global = get_node("/root/globals")
 	var players = global.players # gets the array of players from global
-	var player0 = players[0] # gets the first player
-	player0.IlluminatiCard = players[0].IlluminatiCard.getname()
-	var path = "res://Card Assets Folder/" + player0.IlluminatiCard + ".JPG" # path of their Illuminati Card
+	var path = "res://Card Assets Folder/" + players[0].IlluminatiCard.getname() + ".JPG" # path of their Illuminati Card
 	var image = load(path) 
 	set_texture(image) # displays the Illuminati Card
 
