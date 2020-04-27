2931a14f7f60a93829fd6fdcbbc6b11107c63d7d
S.L <43483411+IamAono@users.noreply.github.com>
2020-04-27 15:06:58 -0700

    took out _ and - from JPG files

---
+++ Illuminati/UndergroundNewspapesr.gd
@@ -0,0 +1,27 @@
+extends Node
+class UndergroundNewspaper:
+	var power = 1;
+	var transPower = 1;
+	var resistance = 5;
+	var income = 0;
+	var money = 0;
+	var up = "out";
+	var right = "in";
+	var down = "none";
+	var left = "none";
+	var alignments = ["communist","liberal"];
+	var type = "group"
+	
+	# Called when the node enters the scene tree for the first time.
+	func _ready():
+		pass # Replace with function body.
+	func attackToControl():
+		pass
+	func attackToDestroy():
+		pass
+	func attackToNeutralize():
+		pass
+	func transferMoney():
+		pass
+	func getName():
+		return("UndergroundNewspapers")
