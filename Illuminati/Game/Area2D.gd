d6a134f777b5c925c91b82e8ac5dc8b84352cb00
richard-pho <55855050+richard-pho@users.noreply.github.com>
2020-04-30 16:51:05 -0700

    update

--- Illuminati/Game/Area2D.gd
+++ Illuminati/Game/Area2D.gd
@@ -4,18 +4,8 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var previous_mouse_position = Vector2()
-var dragging = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-func _process(delta):
-	if dragging && Input.is_action_pressed("drag"):
-		position = get_global_mouse_position()
 
-
-func _on_Area2D_mouse_entered():
-	dragging = true
-
-func _on_Area2D_mouse_exited():
-	dragging = false
