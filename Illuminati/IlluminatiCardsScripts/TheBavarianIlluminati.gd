fbeb32896c47de6a4e7014620bdb6bbd294f4055
S.L <43483411+IamAono@users.noreply.github.com>
2020-05-01 14:53:53 -0700

    worked on attack to control

--- Illuminati/IlluminatiCardsScripts/TheBavarianIlluminati.gd
+++ Illuminati/IlluminatiCardsScripts/TheBavarianIlluminati.gd
@@ -16,7 +16,14 @@ class TheBavarianIlluminati:
 	func _ready():
 		pass # Replace with function body.
 	func attackToControl():
-		pass
+		var global = get_node("/root/globals")
+		var defender = global.defender
+		if global.attackerRoll == 11 or global.attackerRoll == 12:
+			print("Attack failed.")
+		elif (global.roll <= (power - defender.resistance)):
+			print("Attack is successful.")
+		else:
+			print("Attack failed.")
 	func attackToDestroy():
 		pass
 	func attackToNeutralize():
