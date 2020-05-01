bf1ddc8cbbe71440a18ceeba5505de2840a01944
S.L <43483411+IamAono@users.noreply.github.com>
2020-05-01 15:09:46 -0700

    _

--- Illuminati/IlluminatiCardsScripts/TheSocietyOfAssassins.gd
+++ Illuminati/IlluminatiCardsScripts/TheSocietyOfAssassins.gd
@@ -1,5 +1,5 @@
 extends Node
-class TheSocietyOfAssassins:
+class TheSocietyOfAssassins extends Node:
 	var power = 8;
 	var transPower = 8;
 	var income = 8;
@@ -8,12 +8,19 @@ class TheSocietyOfAssassins:
 	var right = "out";
 	var down = "out";
 	var left = "out";
-	var name = "TheSocietyOfAssassins"
+	var cardName = "TheSocietyOfAssassins"
 	# Called when the node enters the scene tree for the first time.
 	func _ready():
 		pass # Replace with function body.
 	func attackToControl():
-		pass
+		var global = get_node("/root/globals")
+		var defender = global.defender
+		if global.attackerRoll == 11 or global.attackerRoll == 12:
+			print("Attack failed.")
+		elif (global.roll <= (power - defender.resistance)):
+			print("Attack is successful.")
+		else:
+			print("Attack failed.")
 	func attackToDestroy():
 		pass
 	func attackToNeutralize():
@@ -23,7 +30,7 @@ class TheSocietyOfAssassins:
 	func printname():
 		print("TheSocietyOfAssassins")
 	func getname():
-		return name
+		return cardName
 	func updateMoney():
 		money = money + income
 	func getincome():
