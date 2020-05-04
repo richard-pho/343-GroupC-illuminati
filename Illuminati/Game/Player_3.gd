4a3153ecec13afbf80b73d210205219381f5c2e2
Albert Truong <albertoe4788@gmail.com>
2020-05-04 15:32:47 -0700

    bank

--- Illuminati/Game/Player_3.gd
+++ Illuminati/Game/Player_3.gd
@@ -25,7 +25,7 @@ func _ready():
 		for i in testarray[3].groupCards:
 			i.money += i.income
 			
-	print("Bank: "player3.IlluminatiCard.money)
+	print("Bank: " + str(player3.IlluminatiCard.money))
 	var global = get_node("/root/globals")
 	var players = global.players # gets the array of players from global
 	var path = "res://Card Assets Folder/" + players[3].IlluminatiCard.getname() + ".JPG" # path of their Illuminati Card
